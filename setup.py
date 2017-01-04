a366277ebd6ae9b4040da4fb00c21f77310127a8
bstriner <bstriner@yahoo.com>
2017-01-04 03:05:02 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ from setuptools import setup, find_packages
 setup(name='keras_tqdm',
       version='0.0.1',
       install_requires=['keras', 'tqdm'],
+      author="Ben Striner",
+      url="https://github.com/bstriner/keras-tqdm",
       packages=find_packages())
