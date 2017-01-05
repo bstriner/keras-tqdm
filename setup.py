16de7b2dac3fdef3d570188fdefb8ceb57a2f68a
bstriner <bstriner@yahoo.com>
2017-01-05 16:14:21 -0500

    v1.0.0

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='keras_tqdm',
-      version='0.0.2',
+      version='1.0.0',
       install_requires=['keras', 'tqdm'],
       author="Ben Striner",
+      author_email="bstriner@gmail.com",
       url="https://github.com/bstriner/keras-tqdm",
+      download_url='https://github.com/bstriner/keras-tqdm/tarball/v1.0.0',
+      description="Train Keras models with TQDM progress bars and IPython support",
+      keywords = ['keras', 'tqdm', 'progress', 'progressbar', 'ipython', 'jupyter'],
       packages=find_packages())
