df79a30df9fc18f4a0d981a3701171829d6296f9
bstriner <bstriner@yahoo.com>
2017-01-20 17:46:57 -0500

    Add universal flag and change to 1.0.3

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='keras_tqdm',
-      version='1.0.2',
+      version='1.0.3,
       install_requires=['keras', 'tqdm'],
       author="Ben Striner",
       author_email="bstriner@gmail.com",
       url="https://github.com/bstriner/keras-tqdm",
-      download_url='https://github.com/bstriner/keras-tqdm/tarball/v1.0.2',
+      download_url='https://github.com/bstriner/keras-tqdm/tarball/v1.0.3',
       description="Train Keras models with TQDM progress bars and IPython support",
       keywords=['keras', 'tqdm', 'progress', 'progressbar', 'ipython', 'jupyter'],
       license='MIT',
